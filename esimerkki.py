7c5bfb44ba374142f29ef6bffc2baafc629cd5eb
jannesi-projects <116154575+jannesi-projects@users.noreply.github.com>
2024-09-24 11:00:47 +0300

    fixed code using pylint

--- esimerkki.py
+++ esimerkki.py
@@ -1,26 +1,68 @@
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
+
 # esimerkki.py
 
 def add(a, b):
+    """_summary_
+        adds a and b together
+    """
     return a+b
 
-def subtract(a, b):
-  return a-b
+def subtract(a, b): # Subtracts b from a
+    """_summary_
+        adds a and b together
+    """
+    return a-b
 
 def multiply(a, b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return a * b
-    print("Tätä ei koskaan tulosteta")  # Suorittamaton koodi
 
 class OmaLuokka:
+    """_summary_
+    """
     def __init__(self, x):
+        """_summary_
+
+        Args:
+            x (_type_): _description_
+        """
         self.x = x
 
     def double_value(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.x * 2
-    
-    def unused_method(self):
-        pass
 
-oma_muuttuja = 5
+    def print_x(self):
+        """_summary_
 
-def unused_function():
-    return "Tätä ei koskaan kutsuta"
+        Args:
+            x (_type_): _description_
+        """
+        print(self.x)
+
+def main():
+    """_summary_
+    """
+    x = 5
+    olio = OmaLuokka(x)
+    print(olio)
+
+if __name__ == '__main__':
+    main()
